04ab9df4e12de66ae97b83e01932d676f3df19e2
Prita-space <pillaiprita3@gmail.com>
2026-07-17 00:02:55 +0530

    Fix double slash and API_URL in Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -23,7 +23,7 @@ function Register() {
 
     try {
       const res = await axios.post(
-        `${API_URL}//api/auth/register`,
+        `${API_URL}/api/auth/register`,
         {
           name,
           email,
