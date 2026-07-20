0a5f5bb2eb0e2e3a0401f520506dd3999115f8af
Prita-space <pillaiprita3@gmail.com>
2026-07-20 22:28:37 +0530

    background

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Logo from "../components/Logo";
-import background from "../assets/Background.jpg";
+import Background from "../assets/Background.jpg";
 import {
   FaEnvelope,
   FaLock,
