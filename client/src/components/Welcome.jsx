eda32fa1bc7bfa029813220f9179c33c237ada8b
Prita-space <pillaiprita3@gmail.com>
2026-07-20 21:41:03 +0530

    bg image change in welcome page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
+import Background from "../assets/Background.jpg";
 import "./welcome.css";
 
 function Welcome() {
